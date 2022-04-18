95f2b95d92dffc6b4ab52169c312fa67ade8279a
Shaz Soto-Munet <shaz.soto.munet@gmail.com>
2022-04-18 14:12:45 -0500

    Finalize exercise.

--- albums_migration.sql
+++ albums_migration.sql
@@ -1,7 +1,7 @@
 USE codeup_test_db;
 
 CREATE TABLE IF NOT EXISTS albums (
-    id AUTO_INCREMENT UNSIGNED INT,
+    id INT UNSIGNED AUTO_INCREMENT,
     artist VARCHAR(50),
     name VARCHAR(100),
     release_date DATE,
