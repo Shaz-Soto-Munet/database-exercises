8d2d00c3a13f18a7980b49a3958abf834fad6558
Shaz Soto-Munet <shaz.soto.munet@gmail.com>
2022-04-19 11:20:27 -0500

    Update basic statements exercise.

--- select_exercises.sql
+++ select_exercises.sql
@@ -1,3 +1,5 @@
 USE codeup_test_db;
 
-SELECT * FROM albums WHERE artist = 'Pink Floyd';
+SELECT 'The name of all albums by Pink Floyd' as caption, name FROM albums WHERE artist = 'Pink Floyd';
+
+SELECT 'The genre of Thriller by Michael Jack is: ' as caption, genre FROM albums WHERE name = 'Thriller';
