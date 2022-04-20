5cee754c24781ea8c064ae315424757b71699961
Shaz Soto-Munet <shaz.soto.munet@gmail.com>
2022-04-20 13:57:34 -0500

    Finalize Order By exercise.

--- group_by_exercises.sql
+++ group_by_exercises.sql
@@ -9,11 +9,13 @@ WHERE last_name LIKE 'E%'
     AND last_name LIKE '%E'
 GROUP BY last_name;
 
+
 SELECT last_name
 FROM employees
 WHERE last_name LIKE 'E%'
     AND last_name LIKE '%E'
 GROUP BY first_name, last_name;
+
 
 SELECT last_name
 FROM employees
@@ -27,15 +29,13 @@ WHERE last_name LIKE '%Q%'
   AND last_name NOT LIKE '%qu%'
 GROUP BY last_name;
 
+
+# 7
 SELECT gender, COUNT(*)
 FROM employees
 WHERE first_name = 'Irena'
     OR first_name = 'Vidya'
     OR first_name = 'Maya'
-GROUP BY gender;
-
-SELECT COUNT(*), gender FROM employees
-WHERE first_name IN ('Irena', 'Vidya', 'Maya')
 GROUP BY gender;
 
 SELECT COUNT(*), gender FROM employees
