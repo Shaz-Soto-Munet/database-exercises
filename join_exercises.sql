bb382c2cfeff2e6a48b315db992d62681005146e
Shaz Soto-Munet <shaz.soto.munet@gmail.com>
2022-04-22 10:29:58 -0500

    FInalize sub-queries exercise.

--- join_exercises.sql
+++ join_exercises.sql
@@ -1,12 +1,15 @@
 USE employees;
 
-SELECT d.dept_name, CONCAT(e.first_name, ' ', e.last_name) AS full_name
+SELECT d.dept_name AS Department_Name, CONCAT(e.first_name, ' ', e.last_name) AS Department_Manager
 FROM employees e
         JOIN dept_emp de ON de.emp_no = e.emp_no
         JOIN departments d ON d.dept_no = de.dept_no
         JOIN dept_manager dm on e.emp_no = dm.emp_no
 WHERE dm.to_date = '9999-01-01'
-ORDER BY full_name DESC;
+ORDER BY Department_Manager DESC;
+
+
+
 
 SELECT d.dept_name, CONCAT(e.first_name, ' ', e.last_name) AS full_name
 FROM employees e
@@ -15,6 +18,9 @@ FROM employees e
         JOIN dept_manager dm ON e.emp_no = dm.emp_no
 WHERE dm.to_date = '9999-01-01' AND e.gender = 'F'
 ORDER BY full_name DESC;
+
+
+
 
 SELECT t.title AS title, COUNT(*) AS Total
 FROM titles t
@@ -26,14 +32,17 @@ WHERE d.dept_no = 'd009'
 GROUP BY t.title;
 
 
+
+
+
 SELECT d.dept_name,
        CONCAT(e.first_name, ' ', e.last_name) AS full_name,
        s.salary AS Salary
 FROM employees e
         JOIN dept_emp de ON de.emp_no = e.emp_no
         JOIN departments d ON d.dept_no = de.dept_no
-        JOIN dept_manager dm on e.emp_no = dm.emp_no
-        JOIN salaries s on e.emp_no = s.emp_no
+        JOIN dept_manager dm ON e.emp_no = dm.emp_no
+        JOIN salaries s ON e.emp_no = s.emp_no
 WHERE dm.to_date = '9999-01-01' AND s.to_date = '9999-01-01'
 ORDER BY full_name DESC;
 
